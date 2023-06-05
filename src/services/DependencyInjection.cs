49b1f14f3bfd1e02f547befc57932fe72faa86af
Redacted Crab <lego@live.ie>
2023-06-05 02:25:03 +0100

    Feature/dependabot (#11)
    * Updated yml for dependabot
    
    * Testing bot
    
    * test
    
    * codeql.yml
    
    * Change bot day to run
    
    * Updated day
    
    * Removed comments and whitespace

--- src/services/DependencyInjection.cs
+++ src/services/DependencyInjection.cs
@@ -19,23 +19,19 @@ namespace Amelia.CEXPOC.DipendencyInjection
             return services.BuildServiceProvider();
         }
 
-        // Class here to setup logging
         private static void LoggingSetup(IServiceCollection services)
         {
-            // Configure Serilog
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Information()
                 .WriteTo.Console()
                 .WriteTo.File("logs/CEX_POC-.txt", rollingInterval: RollingInterval.Day)
                 .CreateLogger();
 
-            // Add logging
             services.AddLogging(builder =>
             {
                 builder.AddSerilog(Log.Logger);
             });
 
-            // Registers all classes that inherit from the BaseApi class
             var baseApiType = typeof(BaseApi<>);
             var baseApiImplementations = Assembly.GetExecutingAssembly().GetTypes()
                 .Where(t => t.BaseType != null && t.BaseType.IsGenericType && t.BaseType.GetGenericTypeDefinition() == baseApiType);
